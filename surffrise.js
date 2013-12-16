5c2d85be08d118302c82d614e5f1d4842b80079b
Nhan <nhanltn@gmailc.om>
2013-12-16 17:51:55 +0900

    https key-cert

--- surffrise.js
+++ surffrise.js
@@ -2,8 +2,8 @@ var https = require('https');
 var fs = require('fs');
 
 var option = {
-	key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-	cert: fs.readFileSync('test/fixtures/keys/agent2-key.pem')
+	key: fs.readFileSync('test/fixtures/keys/private-key.pem'),
+	cert: fs.readFileSync('test/fixtures/keys/certificate.pem')
 };
 
 var server = https.createServer(option, function(req, res) {
